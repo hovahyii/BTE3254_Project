8a221ea420b709de8a711ab4dbd523e41b9b3bea
Hovah Yii <hovahyii@gmail.com>
2023-09-04 17:42:12 +0800

    update readme
    Signed-off-by: Hovah Yii <hovahyii@gmail.com>

---
+++ C program/C program/main.c
@@ -0,0 +1,363 @@
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include "lcd.h"
+
+// Ultrasonic sensor pins
+#define TRIGGER_PIN PA6
+#define ECHO_PIN PA7
+
+volatile uint16_t fanSpeed = 0;
+
+void initUltrasonic() {
+	// Set trigger pin as output
+	DDRA |= (1 << TRIGGER_PIN);
+	// Set echo pin as input
+	DDRA &= ~(1 << ECHO_PIN);
+}
+
+uint16_t measureDistance() {
+	// Trigger ultrasonic sensor
+	PORTA |= (1 << TRIGGER_PIN);
+	_delay_us(10);
+	PORTA &= ~(1 << TRIGGER_PIN);
+
+	// Measure distance
+	uint16_t pulse_width = 0;
+	
+	// Wait for the rising edge
+	while (!(PINA & (1 << ECHO_PIN))) {
+		// Do nothing
+	}
+	
+	// Measure the pulse width (time taken by the sound wave to return)
+	while (PINA & (1 << ECHO_PIN)) {
+		pulse_width++;
+		_delay_us(1);
+	}
+
+	// Calculate distance in centimeters
+	uint16_t distance_cm = (pulse_width * 10) / 96;
+
+	return distance_cm;
+}
+
+
+void adc_init() {
+	ADMUX = (1 << REFS0); // Set reference voltage to AVcc and left-justify result
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // ADC Enable and prescaler of 128
+}
+
+uint16_t adc_read(uint8_t ch) {
+	ADMUX = (ADMUX & 0xF8) | (ch & 0x07); // Clear the channel selection bits and select the desired channel
+	ADCSRA |= (1 << ADSC); // Start single conversion by setting ADSC
+	while (ADCSRA & (1 << ADSC)); // Wait for conversion to complete
+	return ADC; // Read and return the ADC result
+}
+
+void pwm_init() {
+	// Initialize timer0 in PWM mode
+	TCCR0 |= (1 << WGM00) | (1 << COM01) | (1 << WGM01) | (1 << CS00);
+	TCCR2 |= (1 << WGM00) | (1 << COM01) | (1 << WGM01) | (1 << CS00);
+	// Make sure to make OC0,OC2 pin as output pin
+	DDRA = 0xFF;
+	PORTA = 0X00;
+	DDRB = 0XFF;
+}
+
+void led_init() {
+	DDRC |= (1 << PC0) | (1 << PC1) | (1 << PC2); // Set PC0, PC1, PC2 as output pins for LEDs
+	PORTC &= ~((1 << PC0) | (1 << PC1) | (1 << PC2)); // Turn off LEDs initially
+}
+
+void external_interrupt_init() {
+	// External interrupt (INT7) initialization code
+	EICRB |= (1 << ISC71) | (1 << ISC70); // Trigger on rising edge
+	EIMSK |= (1 << INT7); // Enable external interrupt INT7
+}
+
+void lcd_display_welcome() {
+	lcd_clrscr();
+	lcd_gotoxy(0, 0);
+	lcd_puts("Welcome Home,");
+	lcd_gotoxy(0, 1);
+	lcd_puts("Master");
+	_delay_ms(2000);
+	lcd_clrscr();
+}
+
+void lcd_display_detection() {
+	lcd_gotoxy(0, 0);
+	lcd_puts("Human detected.");
+	lcd_gotoxy(0, 1);
+	lcd_puts("Auto Switch On");
+	_delay_ms(1000);
+}
+
+void lcd_display_temperature_fan(int temp) {
+	lcd_clrscr();
+	char buffer[16]; // Create a buffer to hold the string
+	sprintf(buffer, "Room Temp: %d C", temp); // Format temperature
+	lcd_puts(buffer);
+
+	lcd_gotoxy(0, 1);
+	sprintf(buffer, "Fan Speed: %d%%", fanSpeed); // Format fan speed
+	lcd_puts(buffer);
+
+	_delay_ms(1000);
+}
+
+void lcd_display_no_detection() {
+	lcd_clrscr();
+	lcd_gotoxy(0, 0);
+	lcd_puts("No one detected,");
+	lcd_gotoxy(0, 1);
+	lcd_puts("Auto Switch off");
+	_delay_ms(500);
+}
+
+ISR(INT7_vect) {
+	// Display a message on the LCD when the interrupt is activated
+	lcd_clrscr();
+	lcd_gotoxy(0, 0);
+	lcd_puts("Interrupt detected,");
+	lcd_gotoxy(0, 1);
+	lcd_puts("Auto Switch off");
+	PORTC &= ~((1 << PC0) | (1 << PC1) | (1 << PC2) ); // Turn off LEDs
+	PORTA &= ~((1 << PA0) | (1 << PA1) | (1 << PA2) | (1 << PA3)); // Turn off fans
+	_delay_ms(5000);
+}
+
+void normalMode(){
+		lcd_display_welcome();
+
+		// Turn on LEDs and display a message
+		PORTC |= (1 << PC0) | (1 << PC1) | (1 << PC2); // Turn on LEDs
+		lcd_display_detection();
+
+		// Read temperature from LM35 (ADC0/PF0)
+		int adcValue = adc_read(0); // Read from ADC0/PF0
+
+		// Calculate temperature in degrees Celsius
+		// The LM35 has a sensitivity of 10 mV/°C and a reference voltage of 5V (AVcc)
+		float temp = (adcValue / 1024.0) * 500.0; // 500 mV/°C
+
+		// Control fan speed based on temperature as you did before
+		if (temp <= 20) {
+			PORTA = 0x00;
+			OCR0 = 0;
+			OCR2 = 0;
+			fanSpeed = 0; // Set fan speed to 0%
+			} else if ((temp >= 21) && (temp <= 30)) {
+			PORTA = 0x05;
+			OCR0 = 155;
+			OCR2 = 155;
+			fanSpeed = 50; // Set fan speed to 50%
+			} else if ((temp >= 31) && (temp <= 50)) {
+			PORTA = 0x05;
+			OCR0 = 255;
+			OCR2 = 255;
+			fanSpeed = 100; // Set fan speed to 100%
+			} else {
+			PORTA = 0x00;
+			OCR0 = 0;
+			OCR2 = 0;
+			fanSpeed = 0; // Turn off fan
+			lcd_clrscr();
+			lcd_gotoxy(0, 0);
+			lcd_puts("Invalid Temp");
+		}
+		
+		// Display temperature on LCD
+		lcd_display_temperature_fan((int)temp);
+}
+
+
+int main(void) {
+	// Initialization code for peripherals
+	adc_init();
+	pwm_init();
+	initUltrasonic(); // Initialize ultrasonic sensor
+	lcd_init(LCD_DISP_ON);
+	led_init();
+	external_interrupt_init();
+	DDRB = 0x00;
+	PORTB = 0xFF;
+	
+	sei(); // Enable global interrupts
+
+	while (1) {
+
+			uint16_t distance =  measureDistance(); // Calculate distance in centimeters
+			
+				if (distance <= 50) {
+
+					
+					if (PINB == 0xFE) { // Button 1 (PB0)
+						PORTC &= ~(1 << PC0); // Turn off LED1 (PC0)
+						PORTA = 0x05;
+						lcd_clrscr();
+						lcd_puts("LED1 Switched Off");
+						_delay_ms(50);
+					}
+					else if (PINB == 0xFD) { // Button 2 (PB1)
+						PORTC &= ~(1 << PC1); // Turn off LED2 (PC1)
+						PORTA = 0x0F;
+						lcd_clrscr();
+						lcd_puts("LED2 Switched Off");
+						_delay_ms(50);
+
+					}
+					else if (PINB == 0xFB) { // Button 3 (PB2)
+						PORTC &= ~(1 << PC2); // Turn off LED3 (PC2)
+						PORTA = 0x05;
+						lcd_clrscr();
+						lcd_puts("LED3 Switched Off");
+						_delay_ms(50);
+
+					}
+					else if (PINB == 0xF7) { // Button 4 (Fan Control) (PB3) 
+						// Implement code to control the fan based on button press
+						// Example: Change fanSpeed variable, update fan speed
+						PORTA = 0x00;
+						lcd_clrscr();
+						lcd_puts("Fan Switched Off");
+						_delay_ms(50);
+					}
+					else if (PINB == 0xFC) { // Turn off LED1 (PC0) and LED2 (PC1)
+						PORTC = 0x04; // Turn off LED1 (PC0)
+						PORTA = 0x05;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("LED1, LED2");
+						lcd_gotoxy(0, 1);
+						lcd_puts("Switched Off");
+						_delay_ms(50);
+		
+					} 
+					else if (PINB == 0xFA) { 
+						PORTC = 0x0A;  // Turn off LED1 (PC0) and LED3 (PC2)
+						PORTA = 0x05;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("LED1, LED3");
+						lcd_gotoxy(0, 1);
+						lcd_puts("Switched Off");
+						_delay_ms(50);
+
+					}
+					else if (PINB == 0xF9) { 
+						PORTC = 0x09;  // Turn off LED2 and LED3 (PC2)
+						PORTA = 0x05;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("LED1, LED3");
+						lcd_gotoxy(0, 1);
+						lcd_puts("Switched Off");
+						_delay_ms(50);
+
+					}
+					else if (PINB == 0xF8) { // Turn off all LEDs
+						PORTC = 0x08; 
+						PORTA = 0x05;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("Switched Off");
+						lcd_gotoxy(0, 1);
+						lcd_puts("All LEDs");
+						_delay_ms(50);
+
+					}
+					else if (PINB == 0xF6) { //  Turn off LED1 (PC0) and Button 4 (Fan Control) (PB3) 
+						PORTC = 0x06;
+						PORTA = 0x00;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("Switched Off");
+						lcd_gotoxy(0, 1);
+						lcd_puts("LED1, Fans");
+						_delay_ms(50);
+					}
+					else if (PINB == 0xF5) { //  Turn off LED2 (PC1) and Button 4 (Fan Control) (PB3)
+						PORTC = 0x06;
+						PORTA = 0x00;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("Switched Off");
+						lcd_gotoxy(0, 1);
+						lcd_puts("LED2, Fans");
+						_delay_ms(50);
+					}
+					else if (PINB == 0xF3) { //  Turn off LED3 (PC2) and Button 4 (Fan Control) (PB3)
+						PORTC = 0x06;
+						PORTA = 0x00;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("Switched Off");
+						lcd_gotoxy(0, 1);
+						lcd_puts("LED3, Fans");
+						_delay_ms(50);
+					}
+					else if (PINB == 0xF4) { //  Turn off LED1, LED2 and Button 4 (Fan Control) (PB3)
+						PORTC = 0x04;
+						PORTA = 0x00;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("Switched Off");
+						lcd_gotoxy(0, 1);
+						lcd_puts("LED1, LED2, Fans");
+						_delay_ms(50);
+					}
+					else if (PINB == 0xF1) { //  Turn off LED2, LED3 and Button 4 (Fan Control) (PB3)
+						PORTC = 0x01;
+						PORTA = 0x00;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("Switched Off");
+						lcd_gotoxy(0, 1);
+						lcd_puts("LED2, LED3, Fans");
+						_delay_ms(50);
+					}
+					else if (PINB == 0xF2) { //  Turn off LED1, LED3 and Button 4 (Fan Control) (PB3)
+						PORTC = 0x02;
+						PORTA = 0x00;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("Switched Off");
+						lcd_gotoxy(0, 1);
+						lcd_puts("LED1, LED3, Fans");
+						_delay_ms(50);
+					}
+					else if (PINB == 0xF0) { //  Turn off LED1, LED2, LED3 and Button 4 (Fan Control) (PB3)
+						PORTC = 0x00;
+						PORTA = 0x00;
+						lcd_clrscr();
+						lcd_gotoxy(0, 0);
+						lcd_puts("Switched Off all");
+						lcd_gotoxy(0, 1);
+						lcd_puts("Fans and LEDs");
+						_delay_ms(50);
+					}
+					else {
+						normalMode();
+					}
+					
+					
+					
+				} else {
+					// Turn off LEDs and display a message
+					PORTC &= ~((1 << PC0) | (1 << PC1) | (1 << PC2) ); // Turn off LEDs
+					PORTA &= ~((1 << PA0) | (1 << PA1) | (1 << PA2) | (1 << PA3)); // Turn off fans
+					lcd_display_no_detection();
+				}
+			
+			
+	
+		_delay_ms(500); // Delay for smoother operation
+	}
+		
+	return 0; // Return 0 to indicate successful execution (optional)
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Smart House System
 
-<img src="/screenshot.png">
+<img src="./screenshot.png">
 
 The Smart House System is designed to enhance home automation by incorporating various sensors and devices to provide monitoring and control features. This system integrates an LM35 Temperature Sensor, Ultrasonic Sensor (HC-SR04), LCD Display (LM016L), LED control, and a DC Motor (L293D). The system enables temperature monitoring, distance sensing, and control over appliances.
 
@@ -29,26 +29,28 @@ The Smart House System is designed to enhance home automation by incorporating v
 4. **Visual Feedback:**
 - LED indicators provide visual feedback for various states of the system.
 - The LCD Display presents data clearly for monitoring purposes.
+
 <br/>
+
 ## Project Benefits
-<br/>
+
 The Smart House System with integrated LM35, Ultrasonic Sensor, LCD Display, and Motor Control offers the following benefits:
-<br/>
+
 - Real-time temperature monitoring for comfort control.
 - Accurate distance sensing for security and automation.
 - Appliance automation through motor control.
 - User-friendly visual interface through the LCD Display.
 - Visual feedback through LEDs for system status.
-<br/>
+
 This project showcases the potential of integrating various sensors and devices into a smart home system, offering convenience and control for homeowners.
-<br/>
+
 ## Hardware Connections of AtMega64
-<br/>
+
 ### LM35 Temperature Sensor:
 - Connect LM35's Vcc to a 5V pin.
 - Connect LM35's GND to a ground pin.
 - Connect LM35's output to PF0/ADC0.
-<br/>
+
 ### LCD (LM016L):
 - Connect LCD's VDD to a 5V pin.
 - Connect LCD's VSS and VEE to a ground pin.
@@ -56,7 +58,7 @@ This project showcases the potential of integrating various sensors and devices 
 - Connect LCD's RW pin to PD5.
 - Connect LCD's E pin to PD6.
 - Connect LCD's data lines (D0-D7) to digital I/O pins (PD0 to PD3).
-<br/>
+
 ### DC Motor (L293D):
 - Connect L293D's VSS to a 5V pin.
 - Connect L293D's VS to VM (VM connects to 12V).
@@ -66,29 +68,28 @@ This project showcases the potential of integrating various sensors and devices 
 - Connect L293D's OUT2 to Motor-.
 - EN1 connects to PWM1.
 - EN2 connects to PWM2.
-<br/>
+
 ### External Interrupt:
 - Connect the interrupt source (e.g., switch) to PE7.
 - Connect the GND of the switch to ground.
-<br/>
+
 ### Ultrasonic Sensor (HC-SR04):
 - Connect the sensor's Vcc and Trig to a 5V pin.
 - Connect the sensor's GND to ground.
 - Connect the sensor's TR to PA6.
 - Connect the sensor's ECHO to PA7.
-<br/>
+
 ### LEDs:
 - LED1 connects to PC0.
 - LED2 connects to PC1.
 - LED3 connects to PC2.
-<br/>
+
 ### Buttons (to control interrupt and LEDs):
 - Button to control when interrupt is 0 to ground.
 - Button1 (PB0) to control LED1.
 - Button2 (PB0) to control LED2.
 - Button3 (PB0) to control LED3.
 - Button4 (PB0) to control the motor (on/off).
-
 
 <br/>
 
